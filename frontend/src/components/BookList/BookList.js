7ecbd69371033a467b6e755e5490e9c5771baa1c
Vasily Pudikov <vasilypu@mail.ru>
2025-04-20 07:29:00 +0300

    Delete book by id

--- frontend/src/components/BookList/BookList.js
+++ frontend/src/components/BookList/BookList.js
@@ -1,7 +1,14 @@
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import './BookList.css'
+import { deleteBook } from '../../redux/books/actionCreators'
+
 const BookList = () => {
   const books = useSelector((state) => state.books)
+  const dispatch = useDispatch()
+
+  const handleDeleteBook = (id) => {
+    dispatch(deleteBook(id))
+  }
 
   return (
     <div className='app-block book-list'>
@@ -14,6 +21,11 @@ const BookList = () => {
             <li key={book.id}>
               <div className='book-info'>
                 {++i}. {book.title} by <strong> {book.author}</strong>
+              </div>
+              <div className='book-actions'>
+                <button onClick={() => handleDeleteBook(book.id)}>
+                  Delete
+                </button>
               </div>
             </li>
           ))}

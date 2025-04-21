8f2f85ada5f126ab702e6a707420a3850202068f
Vasily Pudikov <vasilypu@mail.ru>
2025-04-21 11:52:31 +0300

    Separate fn for Book creation

--- frontend/src/components/BookForm/BookForm.js
+++ frontend/src/components/BookForm/BookForm.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { useDispatch } from 'react-redux'
-import { v4 as uuidv4 } from 'uuid'
+import createBookWithID from '../../utils/createBookWithID'
 import booksData from '../../data/books.json'
 import './BookForm.css'
 import { addBook } from '../../redux/books/actionCreators'
@@ -14,26 +14,13 @@ const BookForm = () => {
     const randomIndex = Math.floor(Math.random() * booksData.length)
     const randomBook = booksData[randomIndex]
 
-    const randomBookWithId = {
-      ...randomBook,
-      id: uuidv4(),
-      isFavorite: false,
-    }
-
-    dispatch(addBook(randomBookWithId))
+    dispatch(addBook(createBookWithID(randomBook)))
   }
 
   const handleSubmit = (e) => {
     e.preventDefault()
     if (title && author) {
-      const book = {
-        title,
-        author,
-        id: uuidv4(),
-        isFavorite: false,
-      }
-
-      dispatch(addBook(book))
+      dispatch(addBook(createBookWithID({ title, author })))
 
       setTitle('')
       setAuthor('')

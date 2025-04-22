13f5358ac3590ca0bce4607d4be57e053616cf93
Vasily Pudikov <vasilypu@mail.ru>
2025-04-22 13:05:13 +0300

    Filter by author

--- frontend/src/components/BookList/BookList.js
+++ frontend/src/components/BookList/BookList.js
@@ -2,11 +2,15 @@ import { useSelector, useDispatch } from 'react-redux'
 import { BsBookmarkStar, BsBookmarkStarFill } from 'react-icons/bs'
 import './BookList.css'
 import { deleteBook, toggleFavorite } from '../../redux/books/actionCreators'
-import { selectTitleFilter } from '../../redux/slices/filterSlice'
+import {
+  selectTitleFilter,
+  selectAuthorFilter,
+} from '../../redux/slices/filterSlice'
 
 const BookList = () => {
   const books = useSelector((state) => state.books)
   const titleFilter = useSelector(selectTitleFilter)
+  const authorFilter = useSelector(selectAuthorFilter)
   const dispatch = useDispatch()
 
   const handleDeleteBook = (id) => {
@@ -21,7 +25,12 @@ const BookList = () => {
     const matchesTitle = book.title
       .toLowerCase()
       .includes(titleFilter.toLowerCase())
-    return matchesTitle
+
+    const matchesAuthor = book.author
+      .toLowerCase()
+      .includes(authorFilter.toLowerCase())
+
+    return matchesTitle && matchesAuthor
   })
 
   return (
--- frontend/src/redux/slices/filterSlice.js
+++ frontend/src/redux/slices/filterSlice.js
@@ -1,6 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit'
 const initialState = {
   title: '',
+  author: '',
 }
 
 const filterSlice = createSlice({
@@ -10,12 +11,18 @@ const filterSlice = createSlice({
     setTitleFilter: (state, action) => {
       state.title = action.payload
     },
+    setAuthorFilter: (state, action) => {
+      state.author = action.payload
+    },
     resetFilters: (state) => initialState,
   },
 })
 
-export const { setTitleFilter, resetFilters } = filterSlice.actions
+export const { setTitleFilter, setAuthorFilter, resetFilters } =
+  filterSlice.actions
 
 export const selectTitleFilter = (state) => state.filter.title
+
+export const selectAuthorFilter = (state) => state.filter.author
 
 export default filterSlice.reducer

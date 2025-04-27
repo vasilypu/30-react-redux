b09424db355a6165c8cbf0f02432c350ab263ef6
Vasily Pudikov <vasilypu@mail.ru>
2025-04-27 13:52:40 +0300

    Moved IsLoadingViaAPI to redux

--- frontend/src/redux/slices/booksSlice.js
+++ frontend/src/redux/slices/booksSlice.js
@@ -3,7 +3,10 @@ import axios from 'axios'
 import createBookWithID from '../../utils/createBookWithID'
 import { setError } from './errorSlice'
 
-const initialState = []
+const initialState = {
+  books: [],
+  isLoadingViaAPI: false,
+}
 
 export const fetchBook = createAsyncThunk(
   'books/fetchBook',
@@ -13,7 +16,8 @@ export const fetchBook = createAsyncThunk(
       return res.data
     } catch (error) {
       thunkAPI.dispatch(setError(error.message))
-      throw error
+      //throw error
+      return thunkAPI.rejectWithValue(error)
     }
   }
 )
@@ -23,13 +27,16 @@ const booksSlice = createSlice({
   initialState,
   reducers: {
     addBook: (state, action) => {
-      state.push(action.payload)
+      state.books.push(action.payload)
     },
     deleteBook: (state, action) => {
-      return state.filter((book) => book.id !== action.payload)
+      return {
+        ...state,
+        books: state.books.filter((book) => book.id !== action.payload),
+      }
     },
     toggleFavorite: (state, action) => {
-      return state.forEach((book) => {
+      return state.books.forEach((book) => {
         if (book.id === action.payload) {
           book.isFavorite = !book.isFavorite
         }
@@ -38,14 +45,22 @@ const booksSlice = createSlice({
   },
   extraReducers: (builder) => {
     builder.addCase(fetchBook.fulfilled, (state, action) => {
+      state.isLoadingViaAPI = false
       if (action.payload.title && action.payload.author) {
-        state.push(createBookWithID(action.payload, 'API'))
+        state.books.push(createBookWithID(action.payload, 'API'))
       }
+    })
+    builder.addCase(fetchBook.pending, (state) => {
+      state.isLoadingViaAPI = true
+    })
+    builder.addCase(fetchBook.rejected, (state) => {
+      state.isLoadingViaAPI = false
     })
   },
 })
 export const { addBook, deleteBook, toggleFavorite } = booksSlice.actions
 
-export const selectBooks = (state) => state.books
+export const selectBooks = (state) => state.books.books
+export const selectIsLoadingViaAPI = (state) => state.books.isLoadingViaAPI
 
 export default booksSlice.reducer
